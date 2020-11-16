4fba38985a80066b84ed4014fbf90e9574bf9c1c
joseph <43275045+joan-sebas@users.noreply.github.com>
2020-11-16 04:02:56 -0500

    final

--- lib/screens/services.dart
+++ lib/screens/services.dart
@@ -11,21 +11,20 @@ class Services extends StatefulWidget {
 }
 
 class _ServicesState extends State<Services> {
-  List _cities = [
-    "Cluj-Napoca",
-    "Bucuresti",
-    "Timisoara",
-    "Brasov",
-    "Constanta"
+  List _leagues = [
+    "Premier League",
+    "LaLiga",
+    "Bundesliga",
+    "Seria A",
+    "Ligue 1"
   ];
 
   List<DropdownMenuItem<String>> _dropDownMenuItems;
-  String _currentCity;
+  String _currentLeague;
   List<Standings> _standingsList = List<Standings>();
-  List<Tables> _tableList = List<Tables>();
 
   Future<String> _loadProductAsset(String equipo) async {
-    var url = 'http://api.football-data.org/v2/competitions/PL/standings';
+    var url = 'http://api.football-data.org/v2/competitions/$equipo/standings';
 
     var response = await http.get(url,
         headers: {'X-Auth-Token': '2356d455c5194505a9446ec6c3a3ea88'});
@@ -35,16 +34,6 @@ class _ServicesState extends State<Services> {
     }
     return null;
   }
-/*
-Product _parseJsonForCrossword(String jsonString) {
-  Map JSON = json.decode(jsonString);
-  List<Image> words = new List<Image>();
-  for (var word in JSON['across']) {
-    words.add(new Image(word['number'], word['word']));
-  }
-  return new Product(JSON['id'], JSON['name'], new Image(words));
-}
-*/
 
   Future<Soccer> loadProduct(String equipo) async {
     String jsonProduct = await _loadProductAsset(equipo);
@@ -57,8 +46,8 @@ Product _parseJsonForCrossword(String jsonString) {
   @override
   void initState() {
     _dropDownMenuItems = getDropDownMenuItems();
-    _currentCity = _dropDownMenuItems[0].value;
-    loadProduct("Arsenal").then((value) {
+    _currentLeague = _dropDownMenuItems[0].value;
+    loadProduct("PL").then((value) {
       setState(() {
         _standingsList.addAll(value.standings);
       });
@@ -68,7 +57,6 @@ Product _parseJsonForCrossword(String jsonString) {
 
   @override
   Widget build(BuildContext context) {
-    _search();
     return Scaffold(
         appBar: AppBar(
           title: Text('EQUIPOS DE FUTBOL'),
@@ -82,133 +70,132 @@ Product _parseJsonForCrossword(String jsonString) {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    new Text("Please choose your city: "),
+                    new Text("Selecciona la Liga que deseas conocer: "),
                     new Container(
                       padding: new EdgeInsets.all(16.0),
                     ),
                     new DropdownButton(
-                      value: _currentCity,
+                      value: _currentLeague,
                       items: _dropDownMenuItems,
                       onChanged: changedDropDownItem,
                     ),
-                    DataTable(
-                      columns: const <DataColumn>[
-                        DataColumn(
-                          label: Text(
-                            'Equipo',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'PJ',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'G',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'E',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'P',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'GF',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'GE',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'PTS',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                        DataColumn(
-                          label: Text(
-                            'GD',
-                            style: TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        ),
-                      ],
-                      rows: _standingsList[0]
-                          .table // Loops through dataColumnText, each iteration assigning the value to element
-                          .map(
-                            ((element) => DataRow(
-                                  cells: <DataCell>[
-                                    DataCell(Text(element.position.toString() +
-                                        ". " +
-                                        element.team
-                                            .name)), //Extracting from Map element the value
-                                    DataCell(Text(element.pj.toString())),
-                                    DataCell(Text(element.w.toString())),
-                                    DataCell(Text(element.d.toString())),
-                                    DataCell(Text(element.l.toString())),
-                                    DataCell(Text(element.goalsFor.toString())),
-                                    DataCell(
-                                        Text(element.goalsAgainst.toString())),
-                                    DataCell(Text(element.points.toString())),
-                                    DataCell(Text(element.gd.toString())),
-                                  ],
-                                )),
-                          )
-                          .toList(),
-                    ),
+                    _tablaposi()
                   ],
                 ))));
   }
 
-  _search() {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: TextField(
-        decoration:
-            InputDecoration(hintText: 'INGRESA EL EQUIPO (ejemplo: ARSENAL)'),
-        onSubmitted: (texto) {
-          texto = texto.toLowerCase();
-          loadProduct(texto.toString()).then((value) {
-            setState(() {
-              _standingsList.clear();
-              _standingsList.addAll(value.standings);
-              _tableList.clear();
-              for (var standing in _standingsList) {
-                _tableList.addAll(standing.table);
-              }
-            });
-          });
-        },
-      ),
-    );
-  }
-
   List<DropdownMenuItem<String>> getDropDownMenuItems() {
     List<DropdownMenuItem<String>> items = new List();
-    for (String city in _cities) {
-      items.add(new DropdownMenuItem(value: city, child: new Text(city)));
+    for (String league in _leagues) {
+      items.add(new DropdownMenuItem(value: league, child: new Text(league)));
     }
     return items;
   }
 
-  void changedDropDownItem(String selectedCity) {
+  void changedDropDownItem(String selectedLeague) {
     setState(() {
-      _currentCity = selectedCity;
+      _currentLeague = selectedLeague;
+      String _sigla = "PD";
+      if (_currentLeague.compareTo("Premier League") == 0) {
+        _sigla = "PL";
+      } else if (_currentLeague.compareTo("LaLiga") == 0) {
+        _sigla = "PD";
+      } else if (_currentLeague.compareTo("Bundesliga") == 0) {
+        _sigla = "BL1";
+      } else if (_currentLeague.compareTo("Seria A") == 0) {
+        _sigla = "SA";
+      } else if (_currentLeague.compareTo("Ligue 1") == 0) {
+        _sigla = "FL1";
+      }
+
+      loadProduct(_sigla).then((value) {
+        setState(() {
+          _standingsList.clear();
+          _standingsList.addAll(value.standings);
+        });
+      });
     });
+  }
+
+  _tablaposi() {
+    return new DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Text(
+            'Equipo',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'PJ',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'G',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'E',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'P',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'GF',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'GE',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'PTS',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            'GD',
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
+      ],
+      rows: _standingsList[0]
+          .table // Loops through dataColumnText, each iteration assigning the value to element
+          .map(
+            ((element) => DataRow(
+                  cells: <DataCell>[
+                    DataCell(Text(element.position.toString() +
+                        ". " +
+                        element.team
+                            .name)), //Extracting from Map element the value
+                    DataCell(Text(element.pj.toString())),
+                    DataCell(Text(element.w.toString())),
+                    DataCell(Text(element.d.toString())),
+                    DataCell(Text(element.l.toString())),
+                    DataCell(Text(element.goalsFor.toString())),
+                    DataCell(Text(element.goalsAgainst.toString())),
+                    DataCell(Text(element.points.toString())),
+                    DataCell(Text(element.gd.toString())),
+                  ],
+                )),
+          )
+          .toList(),
+    );
   }
 }
